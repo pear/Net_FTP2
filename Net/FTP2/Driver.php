23865b1457488c4abde91122dda2c046a6164c9c
Tobias Schlitt <toby@php.net>
2005-02-23 12:13:50 +0000

    * Changed docblock in Net/FTP2.php to new standard.  * Changed docblock in Net/FTP2/Driver.php to new standard.  * Changed docblock in Net/FTP2/Driver/FTP.php to new standard.  * Added docblock in Net/FTP2/Driver/Socket.php with new standard.  * Added docblock in Net/FTP2/Driver/Stream.php with new standard.
    git-svn-id: http://svn.php.net/repository/pear/packages/Net_FTP2/trunk@180547 c90b9560-bf6c-de11-be94-00142212c4b1

--- Net/FTP2/Driver.php
+++ Net/FTP2/Driver.php
@@ -1,21 +1,30 @@
 <?php
-// +----------------------------------------------------------------------+
-// | Net_FTP2 Version 1.0                                                 |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 2001-2005 Tobias Schlitt                               |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 3.0 of the PHP license,       |
-// | that is available at through the world-wide-web at                   |
-// | http://www.php.net/license/3_0.txt.                                  |
-// | If you did not receive a copy of the PHP license and are unable to   |
-// | obtain it through the world-wide-web, please send a note to          |
-// | license@php.net so we can mail you a copy immediately.               |
-// +----------------------------------------------------------------------+
-// | Authors:       Tobias Schlitt <toby@php.net>                         |
-// +----------------------------------------------------------------------+
-//
-// $Id$
 
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * Net_FTP2_Driver.
+ *
+ * This file holds the implementation of the basis for Net_FTP_Driver_*
+ * classes.
+ *
+ * PHP versions 4 and 5
+ *
+ * LICENSE: This source file is subject to version 3.0 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_0.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category   Networking
+ * @package    FTP2
+ * @author     Tobias Schlitt <toby@php.net>
+ * @copyright  1997-2005 The PHP Group
+ * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @version    CVS: $Id$
+ * @link       http://pear.php.net/package/Net_FTP2
+ * @since      File available since Release 0.0.1
+ */
 // {{{ Error constants
 
 /**
@@ -32,12 +41,16 @@ define('NET_FTP2_ERROR_METHODENOTIMPLEMENTED_TEXT', 'This method is not implemen
  * This class implements common features for Net_FTP2_Driver_* classes,
  * as well as abstract methods, which have to be implemented by the
  * specific driver.
- *  
- * @since 0.1
- * @package Net_FTP2
- * @author    Tobias Schlitt <toby@php.net>
- * @see       http://www.schlitt.info
- * @license   http://www.php.net/license/3_0.txt  PHP License 3.0
+ *
+ * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @category   Networking
+ * @package    FTP
+ * @author     Tobias Schlitt <toby@php.net>
+ * @copyright  1997-2005 The PHP Group
+ * @version    Release: @package_version@
+ * @link       http://pear.php.net/package/Net_FTP
+ * @since      0.0.1
+ * @access     public
  */
 class Net_FTP2_Driver  {
 
---
+++ Net/FTP2/Driver/Socket.php
@@ -0,0 +1,46 @@
+<?php
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * Net_FTP2_Driver_Socket.
+ *
+ * This file holds the driver implementation for Net_FTP2, using the
+ * ext/Socket functions of PHP.
+ *
+ * PHP versions 4 and 5
+ *
+ * LICENSE: This source file is subject to version 3.0 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_0.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category   Networking
+ * @package    FTP2
+ * @author     Tobias Schlitt <toby@php.net>
+ * @copyright  1997-2005 The PHP Group
+ * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @version    CVS: $Id$
+ * @link       http://pear.php.net/package/Net_FTP2
+ * @since      File available since Release 0.0.1
+ */
+
+require_once 'Net/FTP2/Driver.php'
+
+/**
+ * Net_FTP2 driver class for use of ext/Socket.
+ * This class implements a Net_FTP2 driver utilizing ext/Socket.
+ *
+ * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @category   Networking
+ * @package    FTP
+ * @author     Tobias Schlitt <toby@php.net>
+ * @copyright  1997-2005 The PHP Group
+ * @version    Release: @package_version@
+ * @link       http://pear.php.net/package/Net_FTP
+ * @since      0.0.1
+ * @access     public
+ */
+
+ ?>
