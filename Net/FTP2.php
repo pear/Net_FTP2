218dc53fa5ed6d651bfccb22ec14a5428f256291
Daniel O'Connor <clockwerx@php.net>
2008-11-23 03:25:41 +0000

    PHPCS - tabs to spaces, docblocks and other minor cleanup
    git-svn-id: http://svn.php.net/repository/pear/packages/Net_FTP2/trunk@269546 c90b9560-bf6c-de11-be94-00142212c4b1

--- Net/FTP2.php
+++ Net/FTP2.php
@@ -16,14 +16,14 @@
  * the PHP License and are unable to obtain it through the web, please
  * send a note to license@php.net so we can mail you a copy immediately.
  *
- * @category   Networking
- * @package    FTP2
- * @author     Tobias Schlitt <toby@php.net>
- * @copyright  1997-2005 The PHP Group
- * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
- * @version    CVS: $Id$
- * @link       http://pear.php.net/package/Net_FTP2
- * @since      File available since Release 0.0.1
+ * @category  Networking
+ * @package   FTP2
+ * @author    Tobias Schlitt <toby@php.net>
+ * @copyright 1997-2005 The PHP Group
+ * @license   http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @version   CVS: $Id$
+ * @link      http://pear.php.net/package/Net_FTP2
+ * @since     File available since Release 0.0.1
  */
 
 require_once 'PEAR.php';
@@ -33,29 +33,31 @@ require_once 'PEAR.php';
  * This class may not be instanciated directly. Use the factory method
  * Net_FTP2::connect() instead.
  *
- * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
- * @category   Networking
- * @package    FTP
- * @author     Tobias Schlitt <toby@php.net>
- * @copyright  1997-2005 The PHP Group
- * @version    Release: @package_version@
- * @link       http://pear.php.net/package/Net_FTP
- * @since      0.0.1
- * @access     public
+ * @category  Networking
+ * @package   FTP
+ * @author    Tobias Schlitt <toby@php.net>
+ * @copyright 1997-2005 The PHP Group
+ * @license   http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @version   Release: @package_version@
+ * @link      http://pear.php.net/package/Net_FTP
+ * @since     0.0.1
+ * @access    public
  */
-class Net_FTP2 {
+class Net_FTP2
+{
 
     /**
      * Factory method to create a new Net_FTP2 connection. 
      * This factory method returns a Net_FTP2 driver which will be
      * used for all further actions.
      *  
+     * @param string $driver  Name of the driver to use for connection.
+     * @param string $uri     A URI to describe the the FTP connection in the format
+     *                        <protocol>://[<username>][:<password>][@]<host>[:<port>][/<directory]
+     * @param array  $options An array of further options for the FTP connection.
+     *
      * @since 0.1
      * @access public
-     * @param string $driver    Name of the driver to use for connection.
-     * @param string $uri       A URI to describe the the FTP connection in the format
-     *                          <protocol>://[<username>][:<password>][@]<host>[:<port>][/<directory]
-     * @param array  $options   An array of further options for the FTP connection.
      * @static
      * @return object(Net_FTP_Driver_*) The Net_FTP2 driver.
      */
--- generate_package_xml.php
+++ generate_package_xml.php
@@ -1,101 +1,101 @@
 <?php
 
-	$debug = false;
+$debug = false;
 
-    if (isset($argv[1])) {
-        $debug = true;
-    }
-    
-	require_once('PEAR/PackageFileManager.php');
+if (isset($argv[1])) {
+    $debug = true;
+}
 
-	$pkg = new PEAR_PackageFileManager;
+require_once 'PEAR/PackageFileManager.php';
 
-	// directory that PEAR CVS is located in
-	$cvsdir  = '/cvs/pear/';
-	$packagedir = $cvsdir . 'Net_FTP2/';
-	$category = 'Networking';	
+$pkg = new PEAR_PackageFileManager;
 
-	$version = '0.1';
-	
-	$summary = 'Net_FTP2 provides multiple backends and advanced features for communication with FTP servers.';
-	
-	$description = <<<EOT
+// directory that PEAR CVS is located in
+$cvsdir     = '/cvs/pear/';
+$packagedir = $cvsdir . 'Net_FTP2/';
+$category   = 'Networking';
+
+$version = '0.1';
+
+$summary = 'Net_FTP2 provides multiple backends and advanced features for communication with FTP servers.';
+
+$description = <<<EOT
 [[[DESCRIBTION TO BE ADDED HERE]]]
 EOT;
-	
-	$notes = <<<EOT
+
+$notes = <<<EOT
 EOT;
-	
-	$e = $pkg->setOptions(
-		array('baseinstalldir'      => '',
-		      'summary'             => $summary,
-		      'description'         => $description,
-		      'version'             => $version,
-	          'packagedirectory'    => $packagedir,
-	          'pathtopackagefile'   => $packagedir,
-              'state'               => 'devel',
-//              'filelistgenerator'   => 'cvs',
-              'filelistgenerator'   => 'file',
-              'notes'               => $notes,
-			  'package'             => 'Net_FTP2',
-			  'dir_roles'           => array(
-			  		'example'       => 'doc',
-			  		'test'          => 'test'
-              ),
-		      'ignore'              => array(
-                    'package.xml',
-		            'doc*',
-		            'test*', 
-		            'generate_package_xml.php',
-		            '*.tgz',
-		            'FTP_PHP5.php',
-		      ),
-	    )
-    );
-	
-	if (PEAR::isError($e)) {
-    	echo $e->getMessage();
-    	exit;
-	}
-	
-	$e = $pkg->addMaintainer('toby', 'lead', 'Tobias Schlitt', 'toby@php.net');
-	
-	
-	if (PEAR::isError($e)) {
-    	echo $e->getMessage();
-    	exit;
-	}
-	
-    // Orphaned with socket backend
-	// $e = $pkg->addDependency('ftp', null, 'has', 'ext');
-	
-	if (PEAR::isError($e)) {
-    	echo $e->getMessage();
-    	exit;
-	}
-	
-	// hack until they get their shit in line with docroot role
-	$pkg->addRole('tpl', 'php');
-	$pkg->addRole('png', 'php');
-	$pkg->addRole('gif', 'php');
-	$pkg->addRole('jpg', 'php');
-	$pkg->addRole('css', 'php');
-	$pkg->addRole('js', 'php');
-	$pkg->addRole('ini', 'php');
-	$pkg->addRole('inc', 'php');
-	$pkg->addRole('afm', 'php');
-	$pkg->addRole('pkg', 'doc');
-	$pkg->addRole('cls', 'doc');
-	$pkg->addRole('proc', 'doc');
-	$pkg->addRole('sh', 'script');
-	
-	if ($debug) {
-    	$e = $pkg->debugPackageFile();
-	} else {
-    	$e = $pkg->writePackageFile();
-	}
-	
-	if (PEAR::isError($e)) {
-    	echo $e->getMessage();
-	}
+
+$options = array('baseinstalldir'      => '',
+                  'summary'             => $summary,
+                  'description'         => $description,
+                  'version'             => $version,
+                  'packagedirectory'    => $packagedir,
+                  'pathtopackagefile'   => $packagedir,
+                  'state'               => 'devel',
+        //              'filelistgenerator'   => 'cvs',
+                  'filelistgenerator'   => 'file',
+                  'notes'               => $notes,
+                  'package'             => 'Net_FTP2',
+                  'dir_roles'           => array(
+                          'example'       => 'doc',
+                          'test'          => 'test'
+                  ),
+                  'ignore'              => array(
+                        'package.xml',
+                        'doc*',
+                        'test*',
+                        'generate_package_xml.php',
+                        '*.tgz',
+                        'FTP_PHP5.php',
+                  ),
+            );
+
+$e = $pkg->setOptions($options);
+
+if (PEAR::isError($e)) {
+    echo $e->getMessage();
+    exit;
+}
+
+$e = $pkg->addMaintainer('toby', 'lead', 'Tobias Schlitt', 'toby@php.net');
+
+
+if (PEAR::isError($e)) {
+    echo $e->getMessage();
+    exit;
+}
+
+// Orphaned with socket backend
+// $e = $pkg->addDependency('ftp', null, 'has', 'ext');
+
+if (PEAR::isError($e)) {
+    echo $e->getMessage();
+    exit;
+}
+
+// hack until they get their shit in line with docroot role
+$pkg->addRole('tpl', 'php');
+$pkg->addRole('png', 'php');
+$pkg->addRole('gif', 'php');
+$pkg->addRole('jpg', 'php');
+$pkg->addRole('css', 'php');
+$pkg->addRole('js', 'php');
+$pkg->addRole('ini', 'php');
+$pkg->addRole('inc', 'php');
+$pkg->addRole('afm', 'php');
+$pkg->addRole('pkg', 'doc');
+$pkg->addRole('cls', 'doc');
+$pkg->addRole('proc', 'doc');
+$pkg->addRole('sh', 'script');
+
+if ($debug) {
+    $e = $pkg->debugPackageFile();
+} else {
+    $e = $pkg->writePackageFile();
+}
+
+if (PEAR::isError($e)) {
+    echo $e->getMessage();
+}
 ?>
